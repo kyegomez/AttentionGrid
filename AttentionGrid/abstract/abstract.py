83d2297f1b4c6a1a7c34abcbb49d05a0f0f67840
Kye <kye@apacmediasolutions.com>
2023-06-17 09:09:37 -0400

    abstract classes

--- AttentionGrid/abstract/abstract.py
+++ AttentionGrid/abstract/abstract.py
@@ -1,15 +1,30 @@
 from abc import ABC, abstractmethod
 
-
 class AbstractAttention(ABC):
     @abstractmethod
     def forward(self, *args, **kwargs):
+        """ Perform the forward pass of the attention mechanism. """
         pass
 
-class AbstractTransformer(ABC):
+class AbstractEncoder(ABC):
     @abstractmethod
     def forward(self, *args, **kwargs):
+        """ Perform the forward pass of the encoder. """
         pass
 
+class AbstractDecoder(ABC):
+    @abstractmethod
+    def forward(self, *args, **kwargs):
+        """ Perform the forward pass of the decoder. """
+        pass
 
-    
+class AbstractTransformer(AbstractEncoder, AbstractDecoder):
+    """ 
+    An abstract transformer class that inherits from the abstract encoder and decoder.
+    This setup allows the transformer to use the forward method from both the encoder and decoder.
+    """
+
+    @abstractmethod
+    def forward(self, *args, **kwargs):
+        """ Perform the forward pass of the transformer. """
+        pass
